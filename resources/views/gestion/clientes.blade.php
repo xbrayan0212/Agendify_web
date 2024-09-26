b9e81ea8e3063303ccf07d97f5a0daa82594e781
xbrayan0212 <xbrayan.tobii02@gmail.com>
2024-09-26 17:48:15 -0500

    feat: actualiza vista de clientes

--- resources/views/gestion/clientes.blade.php
+++ resources/views/gestion/clientes.blade.php
@@ -1,7 +1,13 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800leading-tight">
-            {{ __('Registrar y Gestionar Clientes') }}
-        </h2>
-    </x-slot>
+    <x-form-create
+    title="Crear Cliente"
+    :ruta="'dashboard'"
+    >
+    @foreach ($atributos as $atributo)
+        <div class="mb-4">
+            <x-input-label for="{{ $atributo }}" class="block text-stone-950 mb-2">{{ ucfirst($atributo) }}:</x-input-label>
+            <input type="text" name="{{ $atributo }}" id="{{ $atributo }}" class="border border-stone-300 rounded-md p-2 w-full" placeholder="{{ ucfirst($atributo) }}">
+        </div>
+    @endforeach
+    </x-form-create>
 </x-app-layout>
