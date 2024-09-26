7e54726a0ac8efe32148c978623364674972eae9
xbrayan0212 <xbrayan.tobii02@gmail.com>
2024-09-26 17:48:12 -0500

    feat: actualiza vista de citas

--- resources/views/gestion/citas.blade.php
+++ resources/views/gestion/citas.blade.php
@@ -1,7 +1,59 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800leading-tight">
-            {{ __('Gestionar Citas') }}
-        </h2>
-    </x-slot>
+    <div class="container"></div>
+    <x-form-create
+        title="Agendar Cita"
+        :ruta="'dashboard'"
+    >
+        <!-- Campos del formulario -->
+        <div class="mb-4">
+            <x-input-label for="cedula" class="block text-stone-950 mb-2">Cédula:</x-input-label>
+            <select name="cedula" id="cedula" onchange="mostrarNombre(this)" class="border border-stone-300 rounded-md p-2 w-full focus:outline-none focus:ring focus:ring-stone-500" required>
+                <option value="">Seleccione una cédula</option>
+                @foreach ($clientes as $cliente)
+                    <option value="{{ $cliente->cedula }}" data-nombre="{{ $cliente->nombre }} {{ $cliente->apellido }}">
+                        {{ $cliente->cedula }}
+                    </option>
+                @endforeach
+            </select>
+        </div>
+
+        <div class="mb-4">
+            <x-input-label for="nombre" class="block text-stone-950 mb-2">Nombre:</x-input-label>
+            <input type="text" id="nombre" name="nombre" class="border border-stone-300 rounded-md p-2 w-full bg-gray-100 cursor-not-allowed" placeholder="Nombre" readonly>
+        </div>
+
+        <div class="mb-4">
+            <x-input-label for="fecha" class="block text-stone-950 mb-2">Fecha:</x-input-label>
+            <input type="date" id="fecha" name="fecha" class="border border-stone-300 rounded-md p-2 w-full focus:outline-none focus:ring focus:ring-stone-500" required>
+        </div>
+
+        <div class="mb-4">
+            <x-input-label for="hora" class="block text-stone-950 mb-2">Hora:</x-input-label>
+            <input type="time" id="hora" name="hora" class="border border-stone-300 rounded-md p-2 w-full focus:outline-none focus:ring focus:ring-stone-500" required>
+        </div>
+
+        <div class="mb-4">
+            <x-input-label for="servicio" class="block text-stone-950 mb-2">Servicio:</x-input-label>
+            <select name="servicio" id="servicio" class="border border-stone-300 rounded-md p-2 w-full focus:outline-none focus:ring focus:ring-stone-500">
+                @foreach ($servicios as $option)
+                    <option value="{{ $option }}">{{ $option }}</option>
+                @endforeach
+            </select>
+        </div>
+
+        <div class="mb-4 ">
+            <x-input-label for="motivo" class="block text-stone-950 mb-2">Motivo:</x-input-label>
+            <input type="text" id="motivo" name="motivo" class="border border-stone-300 rounded-md p-2 w-full" placeholder="Escribe el motivo aquí">
+        </div>
+
+    </x-form-create>
 </x-app-layout>
+
+<script>
+    function mostrarNombre(select) {
+        const selectedOption = select.options[select.selectedIndex];
+        const nombreCompleto = selectedOption.getAttribute('data-nombre');
+        const inputNombre = document.getElementById('nombre');
+        inputNombre.value = nombreCompleto || '';
+    }
+</script>
