1b2a78e8c68e9a0c809356276729ba31b2a0b8b5
xbrayan0212 <xbrayan.tobii02@gmail.com>
2024-09-25 21:51:38 -0500

    fix: actualizar vista del dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,26 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+    <div class="container mx-auto p-4 ">
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4">
+            <!-- Lista de Clientes -->
+            <x-table-component
+                title="Lista de Clientes"
+                :headers="['Nombre', 'Teléfono']"
+                :rows="$clientes->map(fn($cliente) => [$cliente->nombre, $cliente->telefono])->toArray()"
+            />
+
+            <!-- Citas Agendadas -->
+            <x-table-component
+                title="Citas Agendadas"
+                :headers="['Fecha', 'Hora', 'Estado']"
+                :rows="$citas->map(fn($cita) => [$cita->fecha, $cita->hora, $cita->estado])->toArray()"
+            />
+        </div>
+
+        <!-- Historial -->
+        <x-table-component
+            title="Historial"
+            :headers="['Cliente', 'Fecha', 'Observaciones']"
+            :rows="$historial->map(fn($entry) => [$entry->cita->cliente->nombre, $entry->fecha_consulta, $entry->observaciones])->toArray()"
+        />
+    </div>
 </x-app-layout>
