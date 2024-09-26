2ce434e368df4b9c0c2e2475899059869f906077
xbrayan0212 <xbrayan.tobii02@gmail.com>
2024-09-25 21:51:42 -0500

    feat: añadir vistas de gestión

---
+++ resources/views/gestion/citas.blade.php
@@ -0,0 +1,7 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800leading-tight">
+            {{ __('Gestionar Citas') }}
+        </h2>
+    </x-slot>
+</x-app-layout>
