42cb7be0a0e3b083c766200a63c53b63bd4d1981
xbrayan0212 <xbrayan.tobii02@gmail.com>
2024-09-25 21:51:39 -0500

    fix: actualizar rutas en web.php

--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,25 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Gestion\DashboardController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/dashboard', [DashboardController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/calendario', function () {
+    return view('gestion.calendario');
+})->middleware(['auth', 'verified'])->name('calendario');
+
+Route::get('/citas', function () {
+    return view('gestion.citas');
+})->middleware(['auth', 'verified'])->name('citas');
+
+Route::get('/clientes', function () {
+    return view('gestion.clientes');
+})->middleware(['auth', 'verified'])->name('clientes');
+
+
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
