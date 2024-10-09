4140c702e1b584311d06d328ac608c7bcd00a3fc
Brayan Rodriguez <xbrayan.tobii02@gmail.com>
2024-10-09 11:05:41 -0500

    cambio en la estructura de las rutas

--- routes/web.php
+++ routes/web.php
@@ -2,34 +2,10 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Gestion\DashboardController;
-use App\Http\Controllers\Gestion\CitasController;
-use App\Http\Controllers\Gestion\ClientesController;
-use App\Http\Controllers\Gestion\CalendarioController;
-
-Route::get('/dashboard', [DashboardController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
-
-//clientes
-Route::get('/clientes',[ClientesController::class,'index'])->middleware(['auth', 'verified'])->name('clientes');
-Route::post('/clientes/guardar', [ClientesController::class, 'guardar'])->name('clientes.guardar');
-Route::delete('clientes/{id}', [ClientesController::class, 'eliminar'])->name('clientes.eliminar');
-Route::put('cliente/update', [ClientesController::class, 'update'])->name('clientes.update');
-
-//rutas citas
-
-Route::get('/citas', [CitasController::class, 'index'])->middleware(['auth', 'verified'])->name('citas');
-Route::post('citas/guardar', [CitasController::class, 'guardar'])->name('citas.guardar');
-Route::delete('citas/{id}', [CitasController::class, 'eliminar'])->name('citas.eliminar');
-Route::put('citas/update', [CitasController::class, 'update'])->name('citas.update');
-
-//calendario
-Route::get('/calendario', [CalendarioController::class, 'index'])->middleware(['auth', 'verified'])->name('calendario');
-
 
 Route::get('/', function () {
     return view('welcome');
 });
-
 
 
 Route::middleware('auth')->group(function () {
@@ -39,3 +15,7 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+require __DIR__.'/dashboard.php';
+require __DIR__.'/clientes.php';
+require __DIR__.'/citas.php';
+require __DIR__.'/calendario.php';
