b82a9324ebf60896e8b9016e11c76b5bf65be21e
Brayan Rodriguez <xbrayan.tobii02@gmail.com>
2024-10-12 11:41:29 -0500

    Actualizar el layout principal para incorporar nuevos estilos y scripts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,7 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100 ">
+        <div class="min-h-screen bg-gray-100 pb-16 ">
             @include('layouts.navigation')
 
             <!-- Page Heading -->
@@ -32,5 +32,6 @@
                 {{ $slot }}
             </main>
         </div>
+        @include('layouts.footer')
     </body>
 </html>
