c925f6f164c708296dca64e71fa65b30199a594a
Brayan Rodriguez <xbrayan.tobii02@gmail.com>
2024-10-12 11:41:29 -0500

    Actualizar el modelo Historial para incluir nuevos atributos necesarios

--- app/Models/Historial.php
+++ app/Models/Historial.php
@@ -13,11 +13,16 @@ class Historial extends Model
         'fecha_consulta',
         'observaciones',
         'id_cita',
+        'id_profesional'
     ];
 
     public function cita()
     {
         return $this->belongsTo(Cita::class, 'id_cita');
+    }
+    public function profesional()
+    {
+        return $this->belongsTo(User::class, 'id_profesional');
     }
 }
 
