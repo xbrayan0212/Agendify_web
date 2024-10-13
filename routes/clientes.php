d80aca0902a2a0623ae4bdb0adba5e696fb154e0
Brayan Rodriguez <xbrayan.tobii02@gmail.com>
2024-10-13 11:03:27 -0500

    Modifica rutas de clientes

--- routes/clientes.php
+++ routes/clientes.php
@@ -2,8 +2,9 @@
 
 use App\Http\Controllers\Gestion\ClientesController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\ShareNextCita; 
 
-Route::middleware(['auth', 'verified'])->group(function () {
+Route::middleware(['auth', ShareNextCita::class])->group(function () {
     Route::get('/clientes',[ClientesController::class,'index'])->name('clientes');
     Route::post('/clientes/guardar', [ClientesController::class, 'guardar'])->name('clientes.guardar');
     Route::delete('clientes/{id}', [ClientesController::class, 'eliminar'])->name('clientes.eliminar');
