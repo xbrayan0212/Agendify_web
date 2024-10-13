455b3c0a247af828b6b124146d65d9d61eb0c037
Brayan Rodriguez <xbrayan.tobii02@gmail.com>
2024-10-13 11:03:27 -0500

    Modifica rutas de dashboard

--- routes/dashboard.php
+++ routes/dashboard.php
@@ -2,8 +2,9 @@
 
 use App\Http\Controllers\Gestion\DashboardController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\ShareNextCita; 
 
-Route::middleware(['auth', 'verified'])->group(function () {
+Route::middleware(['auth', ShareNextCita::class])->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
     Route::post('/dashboard/gServicio', [DashboardController::class, 'gServicio'])->name('guardar.servicio');
     Route::delete('/dashboard/{id}', [DashboardController::class,'eServicio'])->name('eliminar.servicio');
