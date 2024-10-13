11a2374d53ca0e2cf546dda2e2437ed662cf6a7e
Brayan Rodriguez <xbrayan.tobii02@gmail.com>
2024-10-13 11:03:27 -0500

    Modifica rutas web

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,14 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\ShareNextCita; 
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 
-Route::middleware('auth')->group(function () {
+Route::middleware(['auth', ShareNextCita::class])->group(function (){
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
